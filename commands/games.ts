4df6e621a4fd79e2ed4f1b5ee778cc5b07a9a39b
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-05-16 23:45:15 -0500

    created game embed

--- commands/games.ts
+++ commands/games.ts
@@ -1,10 +1,45 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed } from "discord.js";
+import { fetchGames } from "../lib/fetchGames";
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with Pong!"),
+    .setName("games")
+    .setDescription("Replies with games and scores for the current day"),
   async execute(interaction: any) {
-    return interaction.reply("Pong!");
+    const gameData = await fetchGames();
+
+    const fieldsArray: any = [];
+    gameData.forEach((game: any) => {
+      fieldsArray.push([
+        {
+          name: `${game.teams.home.team.name} vs ${game.teams.away.team.name}`,
+          value: `Start: 7:30PM : Time Remaining: 15:42, Game: 3, Score: ${game.teams.home.score} - ${game.teams.away.score}`,
+        },
+      ]);
+    });
+
+    const exampleEmbed = new MessageEmbed()
+      .setColor("#0099ff")
+      .setTitle("Hockey")
+      // .setURL()
+      .setAuthor({
+        name: "Puckhead Developer",
+        iconURL: "https://i.imgur.com/AfFp7pu.png",
+        url: "https://github.com/jdcheek",
+      })
+      .setDescription(
+        "NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 2022. All Rights Reserved."
+      )
+      .setThumbnail("https://i.imgur.com/AfFp7pu.png")
+      .addFields(fieldsArray.flat())
+      .setImage("https://i.imgur.com/AfFp7pu.png")
+      .setTimestamp()
+      .setFooter({
+        text: "Some footer text here",
+        iconURL: "https://i.imgur.com/AfFp7pu.png",
+      });
+
+    return interaction.reply({ embeds: [exampleEmbed] });
   },
 };
