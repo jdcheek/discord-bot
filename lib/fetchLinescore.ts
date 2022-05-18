3dd7177d118dc03867fe888d4535a0a676493934
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-05-17 22:29:54 -0500

    removed dot notation from json data return to access all available key/values

--- lib/fetchLinescore.ts
+++ lib/fetchLinescore.ts
@@ -3,5 +3,5 @@ export async function fetchLinescore() {
     "https://statsapi.web.nhl.com/api/v1/schedule?expand=schedule.linescore"
   );
   const data = await response.json();
-  return data.dates[0].games;
+  return data;
 }
