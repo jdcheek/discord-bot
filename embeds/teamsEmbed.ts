8805e71fc590e08399a66e712b881dc49fbae519
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-05-18 21:25:18 -0500

    created teams embed

---
+++ embeds/teamsEmbed.ts
@@ -0,0 +1,32 @@
+import { MessageEmbed } from "discord.js";
+
+export function teamsEmbed(teamsData: any) {
+  const fieldsArray: any = [];
+  teamsData.teams.forEach((team: any) => {
+    fieldsArray.push([
+      {
+        name: team.name,
+        value: team.officialSiteUrl,
+        inline: true,
+      },
+    ]);
+  });
+
+  const teamsEmbed = new MessageEmbed()
+    .setColor("#0099ff")
+    .setTitle("NHL Teams:")
+    .setAuthor({
+      name: "jdcheek@github",
+      url: "https://github.com/jdcheek",
+    })
+    .setDescription(
+      "[View my source code!](https://github.com/jdcheek/discord-bot)"
+    )
+    .addFields(fieldsArray.flat())
+    .setTimestamp()
+    .setFooter({
+      text: `${teamsData.copyright}`,
+    });
+
+  return teamsEmbed;
+}
