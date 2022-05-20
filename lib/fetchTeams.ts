98957add492613378043010892cb77ea3bca40d9
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-05-19 23:16:54 -0500

    added modifier to api endpoint for verbose roster results

--- lib/fetchTeams.ts
+++ lib/fetchTeams.ts
@@ -1,5 +1,7 @@
 export async function fetchTeams() {
-  const response = await fetch("https://statsapi.web.nhl.com/api/v1/teams");
+  const response = await fetch(
+    "https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster"
+  );
   const data = await response.json();
   return data;
 }
