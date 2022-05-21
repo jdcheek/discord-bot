5361dede1dd8e29064329b97c326d256ab857266
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-05-20 21:47:21 -0500

    added modifiers to api endpoint

--- lib/fetchTeams.ts
+++ lib/fetchTeams.ts
@@ -1,6 +1,6 @@
 export async function fetchTeams() {
   const response = await fetch(
-    "https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster"
+    "https://statsapi.web.nhl.com/api/v1/teams?expand=team.roster,team.stats,team.schedule.next"
   );
   const data = await response.json();
   return data;
