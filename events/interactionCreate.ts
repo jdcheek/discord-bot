7e58e969a3bff0454574d0fbeb17e7d448b17c0c
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-05-20 21:46:46 -0500

    added interaction type select

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,20 +1,22 @@
 module.exports = {
   name: "interactionCreate",
   async execute(interaction: any) {
-    if (!interaction.isCommand()) return;
+    if (interaction.isCommand()) {
+      const command = interaction.client.commands.get(interaction.commandName);
+      if (!command) return;
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({
+          content: "There was an error while executing this command!",
+          ephemeral: true,
+        });
+      }
+    }
 
-    const command = interaction.client.commands.get(interaction.commandName);
-
-    if (!command) return;
-
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({
-        content: "There was an error while executing this command!",
-        ephemeral: true,
-      });
+    if (interaction.isSelectMenu()) {
+      console.log(interaction);
     }
   },
 };
